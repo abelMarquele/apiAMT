3088346453f5657a24285c739e80c8dac9387ec5
Abel Marquele <aamarquele@gmail.com>
2021-11-23 09:28:51 +0200

    Criar conexao com mysls e fazer a migracao da base de dados

--- apiAMT/settings.py
+++ apiAMT/settings.py
@@ -81,9 +81,17 @@ WSGI_APPLICATION = 'apiAMT.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_amt',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
