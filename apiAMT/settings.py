206a31b47cd3e12705966e8f0ee5097edec5b3d9
Abel Marquele <aamarquele@gmail.com>
2022-02-21 14:31:16 +0200

    Alter o tipo de base de dados para sqlite3

--- apiAMT/settings.py
+++ apiAMT/settings.py
@@ -204,7 +204,6 @@ DATABASES = {
         'PORT':'5432'
     }
 }
-'''
 
 DATABASES = {
     'default': {
@@ -216,7 +215,7 @@ DATABASES = {
         'PORT':'5432'
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
