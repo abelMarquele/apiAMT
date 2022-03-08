e3634190be93c05dc7b5e898602be780772d7c88
Abel Marquele <aamarquele@gmail.com>
2022-03-08 15:26:17 +0200

    verificar as urls que o usuario entrou

--- CSVS/signals.py
+++ CSVS/signals.py
@@ -1,11 +1,16 @@
+from msilib.schema import Class
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
-from .models import Log_login, Profile
+from .models import Log_event, Log_login, Profile
 
 from django.dispatch import receiver
 from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
  
+
+from django.core.signals import request_started, request_finished
+
+#from .tasks import log_request
  
 @receiver(user_logged_in)
 def log_user_login(sender, request, user, **kwargs):
@@ -94,3 +99,32 @@ def user_profile(sender, instance, created, **kwargs):
 		print('Profile created!')
 
 post_save.connect(user_profile, sender=User)
+
+
+def Log_event(request, **kwargs):
+		#if request.user.is_authenticated:
+		print('Abel: ')
+
+@receiver(request_started)
+def log_request(sender, environ, **kwargs):
+	method = environ['REQUEST_METHOD']
+	host = environ['HTTP_HOST']
+	path = environ['PATH_INFO']
+	query = environ['QUERY_STRING']
+	referer = environ['HTTP_REFERER']
+	query = '?' + query if query else ''
+	print('New Request -> {method} {host}{path}{query}'.format(
+					method=method,
+					host=host,
+					path=path,
+					query=query,
+	))
+	print('sender', sender)
+	user_logged_in.connect(Log_event)
+		
+
+# Log_event.objects.create(
+# 	user= sender,
+# 	event_type= method,
+# 	router= referer
+# )
