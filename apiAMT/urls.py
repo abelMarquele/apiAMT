190c1c4cd47ab902bd0ea043f9ffa5e123c91423
Abel Marquele <aamarquele@gmail.com>
2022-09-08 22:14:18 +0200

    Teste

--- apiAMT/urls.py
+++ apiAMT/urls.py
@@ -96,5 +96,6 @@ urlpatterns = [
 
 ]
 
-urlpatterns += [re_path(r'^images/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
+# urlpatterns += [re_path(r'^images/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
