fa9951f17223ad7f285a4690fdb910fb70cd4afe
Abel Marquele <aamarquele@gmail.com>
2022-09-08 14:09:51 +0200

    Permitir que a Imagem estática do Django seja carregando na configuração debug=False v1

--- apiAMT/urls.py
+++ apiAMT/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -95,4 +96,7 @@ urlpatterns = [
 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r'^images/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
