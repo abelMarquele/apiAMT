1a48c17eb128063ebd898dda8028161938feb17e
Abel Marquele <aamarquele@gmail.com>
2022-09-08 12:15:55 +0200

    Tratamento dos erros 400, 403 e 500

--- apiAMT/views.py
+++ apiAMT/views.py
@@ -4,14 +4,14 @@ from django.http import HttpResponse
 
 
 
-# def error_400_view(request, exception):
-# 	return render(request,'dashboard/400.html',  status=400)
+def error_400_view(request, exception):
+	return render(request,'dashboard/400.html',  status=400)
 
-# def error_403_view(request, exception):
-# 	return render(request,'dashboard/403.html',  status=403)
+def error_403_view(request, exception):
+	return render(request,'dashboard/403.html',  status=403)
 
 def error_404_view(request, exception):
 	return render(request,'dashboard/404.html',  status=404)
 
-# def error_500_view(request):
-# 	return render(request,'dashboard/500.html',  status=500)
+def error_500_view(request):
+	return render(request,'dashboard/500.html',  status=500)
