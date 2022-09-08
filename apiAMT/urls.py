8952e31fa1a5244b3a79c1280e37507b8299610a
Abel Marquele <aamarquele@gmail.com>
2022-09-08 15:16:41 +0200

    Resolver o problema do erro 500 da web removendo a urlpatterns

--- apiAMT/urls.py
+++ apiAMT/urls.py
@@ -96,7 +96,5 @@ urlpatterns = [
 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [re_path(r'^images/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
